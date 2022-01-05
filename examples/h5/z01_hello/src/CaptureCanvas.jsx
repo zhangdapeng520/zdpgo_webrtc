fa2e4223d02d18c6d9f842e5648bccc183d5fa81
pygosuperman <pygosuperman@outlook.com>
2022-01-05 17:24:16 +0800

    捕获canvas作为媒体流

--- examples/h5/z01_hello/src/CaptureCanvas.jsx
+++ examples/h5/z01_hello/src/CaptureCanvas.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import '../styles/css/capture-canvas.scss';
 
-//MediaStream对象
-let stream;
-//画布对象
-let canvas;
-//画布2D内容
-let context;
+let stream; // MediaStream对象
+let canvas; // 画布对象
+let context; // 画布2D内容
 
 /**
  * 捕获Canvas作为媒体流示例
@@ -14,64 +11,46 @@ let context;
 class CaptureCanvas extends React.Component {
 
     componentDidMount() {
-        canvas = this.refs['canvas'];
-        this.startCaptureCanvas();
+        canvas = this.refCanvas; // 获取canvas dom元素
+        this.startCaptureCanvas(); // 开始捕获canvas内容
     }
 
     //开始捕获Canvas
     startCaptureCanvas = async (e) => {
-        stream = canvas.captureStream(10);
-        const video = this.refs['video'];
-        //将视频对象的源指定为stream
-        video.srcObject = stream;
-
-        this.drawLine();
+        stream = canvas.captureStream(10); // 捕获canvas流
+        const video = this.refVideo; // 获取视频dom元素
+        video.srcObject = stream; // 将视频对象的源指定为stream
+        this.drawLine(); // 绘制线条
     }
 
     //画线
     drawLine = () => {
-        //获取Canvas的2d内容
-        context = canvas.getContext("2d");
-
-        //填充颜色
-        context.fillStyle = '#CCC';
-        //绘制Canvas背景
-        context.fillRect(0,0,320,240);
-
+        context = canvas.getContext("2d"); // 获取Canvas的2d内容
+        context.fillStyle = '#CCC'; // 填充颜色
+        context.fillRect(0, 0, 320, 240); // 绘制Canvas背景
         context.lineWidth = 1;
-        //画笔颜色
-        context.strokeStyle = "#FF0000";
-
-        //监听画板鼠标按下事件 开始绘画
-        canvas.addEventListener("mousedown", this.startAction);
-        //监听画板鼠标抬起事件 结束绘画
-        canvas.addEventListener("mouseup", this.endAction);
+        context.strokeStyle = "#FF0000"; // 画笔颜色
+        canvas.addEventListener("mousedown", this.startAction); // 监听画板鼠标按下事件 开始绘画
+        canvas.addEventListener("mouseup", this.endAction); // 监听画板鼠标抬起事件 结束绘画
     }
 
     //鼠标按下事件
     startAction = (event) => {
-        //开始新的路径
-        context.beginPath();
-        //将画笔移动到指定坐标，类似起点
-        context.moveTo(event.offsetX, event.offsetY);
-        //开始绘制
-        context.stroke();
-        //监听鼠标移动事件  
-        canvas.addEventListener("mousemove", this.moveAction);
+        context.beginPath(); // 开始新的路径
+        context.moveTo(event.offsetX, event.offsetY); // 将画笔移动到指定坐标，类似起点
+        context.stroke(); // 开始绘制
+        canvas.addEventListener("mousemove", this.moveAction); // 监听鼠标移动事件
     }
 
     //鼠标移动事件  
     moveAction = (event) => {
-        //将画笔移动到结束坐标，类似终点
-        context.lineTo(event.offsetX, event.offsetY);
-        //开始绘制
-        context.stroke();
+        context.lineTo(event.offsetX, event.offsetY); // 将画笔移动到结束坐标，类似终点
+        context.stroke(); // 开始绘制
     }
 
     //鼠标抬起事件 
     endAction = () => {
-        //移除鼠标移动事件
-        canvas.removeEventListener("mousemove", this.moveAction);
+        canvas.removeEventListener("mousemove", this.moveAction);// 移除鼠标移动事件
     }
 
     render() {
@@ -84,13 +63,14 @@ class CaptureCanvas extends React.Component {
                     {/* 画布Canvas容器 */}
                     <div className="small-canvas">
                         {/* Canvas不设置样式 */}
-                        <canvas ref='canvas'></canvas>
+                        <canvas ref={(el) => this.refCanvas = el}/>
                     </div>
-                    <video className="small-video" ref='video' playsInline autoPlay></video>
+                    <video className="small-video" ref={(el) => this.refVideo = el} playsInline autoPlay/>
                 </div>
             </div>
         );
     }
 }
+
 //导出组件
 export default CaptureCanvas;
